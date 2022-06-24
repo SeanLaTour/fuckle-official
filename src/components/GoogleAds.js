386ba44874501972b635b1fefa032a19fd357bf3
Sean La Tourette <sean.latourette@straightfire.tech>
2022-06-23 22:30:33 -0700

    stufjj

--- src/components/GoogleAds.js
+++ src/components/GoogleAds.js
@@ -9,9 +9,10 @@ render () {
     return (
         <ins className='adsbygoogle'
           style={{ display: 'block', width: "250px", height: "60px" }}
-          data-ad-client='ca-pub-4651779567680129'
-          data-ad-slot='6842240717'
-          data-ad-format='auto' />
+          data-ad-client="ca-pub-4651779567680129"
+          data-ad-slot="4626877586"
+          data-ad-format='auto'
+          data-full-width-responsive="true" />
     );
   }
 }
